aa4719baa4fb1c4d0eb3c6fc96707455703d3587
Luna the Nerd Derg <nerddergontoastspam@gmail.com>
2026-08-16 05:17:02 +0000

    Delete Reefbackend directory

--- Reefbackend/cli.h
+++
@@ -1,6 +0,0 @@
-#include "Profile.h"
-//#include <curl/curl.h>
-#include <cpr/cpr.h>
-//using cpr::cpr;
-
-bool cli(bool devMode=false);
--- Reefbackend/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "cli.h"
-
-int main(int argc, char** argv) {
-    std::cin;
-    cli(true); // Change to false to exit dev mode
-    return 0;
-}
